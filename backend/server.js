50289a22946d0e92f4ca08e85dc0c3f815dafa19
Atharv Patil <atharvsp6@gmail.com>
2025-11-06 00:06:10 +0530

    fix: allow patch requests via cors

--- backend/server.js
+++ backend/server.js
@@ -85,7 +85,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 204
 };
